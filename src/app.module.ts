9718e6a3cb184f2ea950fe3bb7ff517739351af2
tox-in <habimanat78@gmail.com>
2024-06-28 20:53:54 +0200

    initials of User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/store'),
     ProductModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
