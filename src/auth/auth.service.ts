f6c18e1c50de66546f1193cb3d991629a1e020ed
tox-in <habimanat78@gmail.com>
2024-06-28 22:02:53 +0200

    initial authentication functionality

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.userService.findUser(username);
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (user && isPasswordMatch) {
+      return user;
+    }
+    return null;
+  }
+}
