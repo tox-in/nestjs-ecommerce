eb30d675fc252b15fd77fb947ceb5d46650bb54c
tox-in <habimanat78@gmail.com>
2024-06-28 21:38:16 +0200

    basic user functionalities(service and module)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schema';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService]
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
