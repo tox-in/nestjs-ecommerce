c6e787af445f3b71e5d708f50067fa21b8b8c837
tox-in <habimanat78@gmail.com>
2024-06-29 11:21:48 +0200

    changes in cartSchema

--- src/cart/schemas/cart.schema.ts
+++ src/cart/schemas/cart.schema.ts
@@ -1,19 +1,19 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { SchemaTypes } from 'mongoose';
-import { Item } from './item.schema';
+import { Document, SchemaTypes } from 'mongoose';
+import { Item, ItemSchema } from './item.schema';
 
 export type CartDocument = Cart & Document;
 
 @Schema()
 export class Cart {
-  @Prop({ type: SchemaTypes.ObjectId, ref: 'User' })
+  @Prop({ type: SchemaTypes.ObjectId, ref: 'User', required: true })
   userId: string;
 
-  @Prop()
+  @Prop({ type: [ItemSchema], default: [] })
   items: Item[];
 
-  @Prop()
+  @Prop({ required: true })
   totalPrice: number;
 }
 
