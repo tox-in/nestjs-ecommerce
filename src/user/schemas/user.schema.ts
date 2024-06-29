f9f387631aa0348d42d161d8dfa62c15fd22c101
tox-in <habimanat78@gmail.com>
2024-06-29 09:54:46 +0200

    roles

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from 'src/auth/enums/role.enum';
 
 export type UserDocument = User & Document;
 
@@ -14,6 +15,9 @@ export class User {
 
   @Prop()
   password: string;
+
+  @Prop()
+  roles: Role[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
